f6ab77fce6603e44e4b00ef2e99935a7a1978e18
OpecenaPalacinka <61805572+OpecenaPalacinka@users.noreply.github.com>
2020-03-13 10:59:29 +0100

    Hotovo

--- DrawingPanel/src/BasicDrawing.java
+++ DrawingPanel/src/BasicDrawing.java
@@ -1,17 +1,28 @@
-import javax.swing.JFrame;
-import java.awt.*;
+import javax.swing.*;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class BasicDrawing extends JFrame{
+public class BasicDrawing extends JFrame {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+        jf.setTitle("A19B0157P");
 
-    public static void main(String[] args){
-        new BasicDrawing().setVisible(true);
-    }
+        DrawingPanel panel = new DrawingPanel();
+        jf.add(panel);
+        jf.pack();
 
-    public BasicDrawing(){
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setTitle("A19B0157P");
-        getContentPane().add(new DrawingPanel());
-        pack();
+        jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jf.setLocationRelativeTo(null);
+        jf.setVisible(true);
+
+        java.util.Timer myTimer = new Timer();
+        myTimer.scheduleAtFixedRate(new TimerTask() {
+
+            @Override
+            public void run() {
+                panel.repaint();
+            }
+        }, 0, 20);
+
     }
 }
--- DrawingPanel/src/DrawingPanel.java
+++ DrawingPanel/src/DrawingPanel.java
@@ -1,23 +1,102 @@
+
 import javax.swing.*;
 import java.awt.*;
-
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
 
 public class DrawingPanel extends JPanel {
 
+    private long startTime = System.currentTimeMillis();
+    private final int W = 640;
+    private final int H = 480;
+    public DrawingPanel() {
+        setPreferredSize(new Dimension(W, H));
+    }
 
     @Override
-
     public void paint(Graphics g) {
-
         super.paint(g);
-
-
 
         Graphics2D g2 = (Graphics2D)g;
 
+        final int GS = 30;
+        g2.setColor(Color.GREEN);
+        g2.fillRect(0,this.getHeight()-GS,this.getWidth(),GS);
+        g2.translate(0,this.getHeight()-GS);
+
+        drawTraktor(g2);
+
+    }
+
+    public void drawTraktor(Graphics2D g2){
+         double r1 = 150;
+         double r2 = 100;
+         double d = 350;
+
+        final double L = 20;
+        final double W = 300;
+        final double H = 400;
+        final double MW = 250;
+        final double MH = 200;
+        double U = r2/2;
+
+        long currTime = System.currentTimeMillis();
+        long elapsed = currTime - startTime;
+        double zacatek = 50*elapsed/1000.0;
+
+        if(zacatek>this.getWidth()){
+            zacatek -= this.getWidth()+r1+r2+d;
+        }
+        g2.translate(zacatek,0);
+
+        g2.setColor(Color.RED);
+        g2.fill(new Rectangle2D.Double(L,-U - H,W,H));
+        g2.fill(new Rectangle2D.Double(L+W,-U - MH,MW,MH));
+
+        drawWheels(r1,r2,d,g2);
+
+    }
+
+    private void drawWheels(double r1, double r2, double d, Graphics2D g2){
+
+        g2.translate(r1,-r1);
+        drawWheel(r1,g2);
+        g2.translate(d,r1-r2);
+        drawWheel(r2,g2);
 
 
+    }
 
+    public void drawWheel(double r, Graphics2D g2){
 
+        g2.setColor(Color.BLACK);
+        g2.fill(new Ellipse2D.Double(-r,-r,2*r,2*r));
+
+        double r2 = 0.8*r; //polomer disku
+        g2.setColor(Color.DARK_GRAY);
+        g2.fill(new Ellipse2D.Double(-r2,-r2,2*r2,2*r2));
+
+        final double r3 = 20; //polomer oje
+        g2.setColor(Color.RED);
+        g2.fill(new Ellipse2D.Double(-r3,-r3,2*r3,2*r3));
+
+        final double d =45;
+        final double rb =5;
+
+        AffineTransform oldTR = g2.getTransform();
+
+        long currTime = System.currentTimeMillis();
+        long elapsed = currTime - startTime;
+        final double SPA = Math.PI;
+
+        g2.rotate(Math.toRadians(45) + SPA * elapsed / 1000.0);
+        for (int i = 0;i<4;i++) {
+            g2.translate(d, 0);
+            g2.fill(new Ellipse2D.Double(-rb, -rb, 2 * rb, 2 * rb));
+            g2.translate(-d, 0);
+            g2.rotate(Math.toRadians(90));
+        }
+        g2.setTransform(oldTR);
     }
 }
