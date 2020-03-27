d194886da704884b8a82c17f8d59c750f62bb817
OpecenaPalacinka <61805572+OpecenaPalacinka@users.noreply.github.com>
2020-03-27 18:05:03 +0100

    Pokračování seminárky

--- Semestralka/waterflowsim_upg_2020-master/src/DrawingPanel.java
+++ Semestralka/waterflowsim_upg_2020-master/src/DrawingPanel.java
@@ -2,83 +2,145 @@ import waterflowsim.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Line2D;
+import java.awt.geom.Path2D;
 import java.awt.geom.Point2D;
-import java.awt.image.BufferedImage;
 
 public class DrawingPanel extends JPanel {
 
-    private int w;
-    private int h;
+    private double minX,maxX;
+    private double minY,maxY;
+    private double hodnotaX, hodnotaY;
+    private int velikostWidth = 1000;
+    private int	velikostHeight = 1000;
+
+    @Override
+    public void paint(Graphics g) {
+        super.paint(g);
+        Graphics2D g2 = (Graphics2D)g;
+
+        computeModel2WindowTransformation(Simulator.getDimension().x,Simulator.getDimension().y);
+
+        int width = this.getWidth();
+        int height = this.getHeight();
+
+        double scaleX = width/hodnotaX;
+        double scaleY = height/hodnotaY;
+        double scale = Math.min(scaleX,scaleY);
+
+        /*
+        double startX = (width - scale*width)/4;
+        double startY = (height - scale*height)/4;
+
+         */
+
+        g2.scale(scale,scale);
+
+        drawWaterFlowState(g2);
+    }
 
     public void drawWaterFlowState(Graphics2D g){
         drawTerrain(g);
         drawWaterLayer(g);
     }
+
     public void drawTerrain(Graphics2D g){
     }
-    public void drawWaterLayer(Graphics2D g){
-        BufferedImage image = new BufferedImage(w,h,BufferedImage.TYPE_3BYTE_BGR);
-        Cell[] cells = Simulator.getData();
-        int[] poleBarev = new int[Simulator.getData().length];
 
-        for (int i = 0; i < Simulator.getData().length;i++){
-            if(cells[i].isDry()){
-                poleBarev[i] = 255;
-            } else {
-                poleBarev[i] = 255*255*255;
+    public void drawWaterLayer(Graphics2D g){
+        computeModelDimensions();
+            drawWaterSources(g);
+            Path2D water = new Path2D.Double();
+            int cellBefore;
+            int cellAfter;
+            g.setColor(Color.BLUE);
+            for(int i = 0; i < maxY-1; i++) {
+                for(int j = 0; j < maxX-1; j++) {
+                    cellBefore = i * (int)maxX + j;
+                    cellAfter = (i + 1) * (int)maxX + (j + 1);
+                    Cell cell = Simulator.getData()[cellBefore];
+                    Cell cell2 = Simulator.getData()[cellAfter];
+                    if(!cell.isDry()) {
+                        if(!cell2.isDry()) {
+                            water.lineTo(j + Simulator.getDelta().x, i + Simulator.getDelta().y);
+                        }else {
+                            water.moveTo(j + Simulator.getDelta().x, i + Simulator.getDelta().y);
+                        }
+                    }else {
+                        water.moveTo(j + Simulator.getDelta().x, i + Simulator.getDelta().y);
+                    }
+                }
             }
+            g.draw(water);
+
         }
 
-        image.setRGB(0,0,w,h,poleBarev,0,w);
-
-        drawWaterSources(g);
-    }
     public void drawWaterSources(Graphics2D g){
         waterflowsim.WaterSourceUpdater[] vodniZdroje = Simulator.getWaterSources();
         for(waterflowsim.WaterSourceUpdater updater: vodniZdroje){
-            drawWaterFlowLabel(updater.getIndex(), Simulator.getGradient(5), updater.getName(), g);
+          Point2D uvodniBod = new Point2D.Double(hodnotaX,hodnotaY);
+            drawWaterFlowLabel(uvodniBod,Simulator.getGradient(updater.getIndex()),updater.getName(),g);
         }
-    }
-    public void drawWaterFlowLabel(Point2D position, Vector2D directionFlow, String name, Graphics2D g){
 
     }
+    public void drawWaterFlowLabel(Point2D position, Vector2D dirFlow, String name, Graphics2D g){
+        double x1 = position.getX();
+        double x2 = position.getX()-dirFlow.x.doubleValue();
+        double y1 = position.getY();
+        double y2 = position.getY()-dirFlow.y.doubleValue();
 
-    public void drawArrow(Graphics2D g2d, double x1, double y1, double x2, double y2, double k, double l){
-        g2d.draw(new Line2D.Double(x1,y1,x2,y2));
-        for(int i = 0; i<100;i+=10) {
-            double ux = x2-x1;
-            double uy = y2-y1;
-            double len = Math.hypot(ux,uy);
-            ux/=len;
-            uy/=len;
-            double nx = uy;
-            double ny = -ux;
-            double cx = x2-ux*l;
-            double cy = y2-uy*l;
-            double d1x = cx+nx*k;
-            double d1y = cy+ny*k;
-            double d2x = cx-nx*k;
-            double d2y = cy-ny*k;
+        // Spocitame slozky vektoru od (x1, y1) k (x2, y2)
+        double vx = x2 - x1;
+        double vy = y2 - y1;
 
-            g2d.draw(new Line2D.Double(d1x-i, d1y-i, x2-i, y2-i));
-            g2d.draw(new Line2D.Double(d2x-i, d2y-i, x2-i, y2-i));
-        }
+        // Spocitame vektoru v, tj usecky od (x1, y1) k (x2, y2).
+        // K vypoctu druhe mocniny idealne pouzivame nasobeni, ne funkci pow
+        // (je mnohem pomalejsi).
+        double vLength = Math.sqrt(vx*vx + vy*vy);
+
+        // Z vektoru v udelame vektor jednotkove delky
+        double vNormX = vx / vLength;
+        double vNormY = vy / vLength;
+
+        // Vektor v protahneme na delku arrowLength
+        double vArrowX = vNormX * 20;
+        double vArrowY = vNormY * 20;
+
+        // Spocitame vektor kolmy k (vx, vy)
+        // Z nej pak odvodime koncove body carek tvoricich sipku.
+        double kx = -vArrowY;
+        double ky = vArrowX;
+
+        // Upravime delku vektoru k, aby byla sipka hezci
+        kx *= 0.25;
+        ky = 0.25;
+
+        // Cara od (x1, y1) k (x2, y2)
+        g.draw(new Line2D.Double(x1, y1, position.getX()+dirFlow.x.doubleValue()*50, position.getY()+dirFlow.y.doubleValue()*50));
+
+        // Sipka na konci
+        g.draw(new Line2D.Double(x2, y2, x2 - vArrowX + kx, y2 - vArrowY + ky));
+        g.draw(new Line2D.Double(x2, y2, x2 - vArrowX - kx, y2 - vArrowY - ky));
     }
 
     public void computeModelDimensions(){
-
+        minX= Simulator.getStart().x;
+        minY= Simulator.getStart().y;
+        maxX=Simulator.getDimension().x.doubleValue();
+        maxY=Simulator.getDimension().y.doubleValue();
     }
+
     public void computeModel2WindowTransformation(int width, int height){
-
+        computeModelDimensions();
+        hodnotaX = maxX-minX;
+        hodnotaY = maxY-minY;
     }
+
     public Point2D model2window(Point2D m){
-
+        computeModel2WindowTransformation((int)hodnotaX,(int)hodnotaY);
+        m.setLocation(hodnotaX,hodnotaY);
+        return m;
     }
 
-    @Override
-    public void paint(Graphics g) {
-        Graphics2D g2 = (Graphics2D)g;
-        computeModel2WindowTransformation();
-        drawWaterFlowState(g2);
-    }
+
+
 }
--- Semestralka/waterflowsim_upg_2020-master/src/L01_SpusteniSimulatoru.java
+++ Semestralka/waterflowsim_upg_2020-master/src/L01_SpusteniSimulatoru.java
@@ -4,41 +4,20 @@ import waterflowsim.Cell;
 
 import javax.swing.*;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class L01_SpusteniSimulatoru extends JFrame {
 
+
 	public static void main(String[] args) {
 		Scenarios[] scenarios = Simulator.getScenarios();
-		
-		// Vypis existujicich scenaru
-		for(Scenarios sc: scenarios) {
-			System.out.println(sc);
-		}
-		
+
 		// Nahrani a spusteni prvniho scenare
 		Simulator.runScenario(0);
 
-		// Ziskani prvni bunky z plochy
-		Cell cell = Simulator.getData()[0];
-
-		// Spusteni simulace, v kazdem kroku cyklu se posune o preddefinovanou
-		// dobu.
-		for(int i=0; i<100; i++) {
-			Simulator.nextStep(0.02);
-			System.out.println(cell);
-		}
-
-		// Ziskani vsech vodnich zdroju
-		// a pruchod pres vsechny zdroje	
-		waterflowsim.WaterSourceUpdater[] zdroje = Simulator.getWaterSources();
-		for(waterflowsim.WaterSourceUpdater up: zdroje){
-			System.out.print(up.getName());
-			System.out.println(" na indexu v poli: "+up.getIndex());
-		}
-
 		JFrame jf = new JFrame();
 		jf.setTitle("A19B0157P");
-
 		DrawingPanel panel = new DrawingPanel();
 		jf.add(panel);
 		jf.pack();
@@ -46,6 +25,17 @@ public class L01_SpusteniSimulatoru extends JFrame {
 		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jf.setLocationRelativeTo(null);
 		jf.setVisible(true);
+
+
+		Timer myTimer = new Timer();
+		myTimer.scheduleAtFixedRate(new TimerTask() {
+
+			@Override
+			public void run() {
+				Simulator.nextStep(0.02);
+				panel.repaint();
+			}
+		}, 0, 100);
 	}
 
 
